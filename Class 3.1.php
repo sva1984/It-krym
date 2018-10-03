84d89916d40222664844b4d06f49a9eabb49fb62
sva1984 <rasev108@gmail.com>
2018-10-03 18:17:32 +0300

    New class 3.1

--- Class 3.1.php
+++ Class 3.1.php
@@ -1,7 +1,9 @@
 <?php
 Class Human {
-
-    public function setLasttName($lastName)
+    private $lastName;
+    private $firstName;
+    private $age
+    public function setLastName($lastName)
     {
         $this->lastName = $lastName;
     }
@@ -9,47 +11,33 @@ Class Human {
     {
         $this->firstName = $firstName;
     }
-    public function setAge($Age){
-        $this->Age = $Age;
+    public function setAge($age){
+        $this->Age = $age;
     }
-    public function getLasttName($lastName)
+    public function getLastName()
     {
         return $this->lastName;
     }
-    public function getFirstName($firstName)
+    public function getFirstName()
     {
         return $this->firstName ;
     }
-    public function getAge($firstName){
-        return $this->Age;
+    public function getAge(){
+        return $this->age;
     }
     public function __toString()
     {
-        return  '!' . $this->firstName .  " " . $this->lastName ." " .  $this->Age;
+        return  '!' . $this->firstName .  " " . $this->getLastName() ." " .  $this->age;
     }
 }
-class Student{
-    public $firstname;
-    public $lastname;
-    public $kurs;
-    public $forma_obucheniya;
-    public $ocenki;
-    public function __toString()
-    {
-        return  '!' . $this->firstname .  " " . $this->lastname ." " .  $this->kurs. ' ' . $this->forma_obucheniya . ' ' . $this->ocenki;
+class Student extends Human {
+    public function getFullName(){
+        return $this->firstName . ' ' . $this->getLastName();
     }
-}
+    }
 $objHuman = new Human();
 $objHuman->setFirstName('Ivan');
-$objHuman->setLasttName('Ivanov');
+$objHuman->setLastName('Ivanov');
 $objHuman->setAge(25);
+echo $objHuman . PHP_EOL;
 
-echo  $objHuman .PHP_EOL;
-$objStudent1= new Student();
-$objStudent1->firstname='Petr';
-$objStudent1->lastname='Petrov';
-$objStudent1->kurs='3';
-$objStudent1->forma_obucheniya='Stacionarnoe obu4enie';
-$objStudent1->ocenki='4, 5, 4, 4';
-
-echo $objStudent1 .PHP_EOL;
